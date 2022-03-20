cecb410287083ff3abb512169c41685a17c24977
7j4n1 <ahmaholy@gmail.com>
2022-03-20 01:30:38 +0100

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+from morse import morse
+
+print("Encoding:")
+encoded_msg = morse().encode('USE')
+print(encoded_msg)
+print("Decoding")
+decoded_msg = morse().decode(encoded_msg)
+print(decoded_msg)
